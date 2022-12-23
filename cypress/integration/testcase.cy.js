2c430ba42a71adc67d5a5d732257287145760605
solssak <gswoong1012@gmail.com>
2022-12-23 23:23:19 +0900

    테스트 코드 작성

--- cypress/integration/testcase.cy.js
+++ cypress/integration/testcase.cy.js
@@ -1,5 +1,3 @@
-const { default: Item } = require("antd/lib/list/Item");
-
 describe("Lotto app", () => {
   beforeEach(() => {
     cy.visit("https://next-step.github.io/js-lotto/");
@@ -7,6 +5,7 @@ describe("Lotto app", () => {
 
   it("input 창에 문자열 입력 시 값이 입력되지 않는다.", () => {});
   it("input 창에 특수문자 입력 시 값이 입력되지 않는다.", () => {});
-  it("로또 구입 금액 입력 시 1000원 단위가 아닌 경우 alert창을 띄운다", () => {});
+  it("엔터 키 입력 시 1000원 단위가 아닌 경우 alert창을 띄운다", () => {});
+  it("엔터 키 입력 시 1000원 단위가 아닌 경우 input값을 비운다", () => {});
   it("구입 금액은 1,000이상 100,000 이하여야한다.", () => {});
 });
