da9a7ccd448273f1a211ecfbd8ef0d555cdcf4cf
solssak <gswoong1012@gmail.com>
2023-01-04 23:47:11 +0900

    기능 구현 list 작성

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,11 @@ const $modalClose = $("modal-close");
 const $modal = $(".modal");
 const $lottoNumbersToggleButton = $(".lotto-numbers-toggle-button");
 
-// 로또 구입 금액을 입력하면, 금액에 해당하는 로또를 발급해야 한다.
-// const lottoPurchase = () => {
-//   const $lottoPurchaseInput = $(".lotto-purchase-input");
-//   const $lottoPurchaseBtn = $(".lotto-numbers-toggle-btn");
-// };
+//로또 구입 금액을 입력하면, 금액에 해당하는 로또를 발급해야 한다.
+const lottoPurchase = () => {
+  const $lottoPurchaseInput = $(".lotto-purchase-input");
+  const $lottoPurchaseBtn = $(".lotto-numbers-toggle-btn");
+};
 
 // const onModalShow = () => {
 //   $modal.classList.add("open");
