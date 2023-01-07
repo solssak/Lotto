bd14901e76e3b90561791d28d8e504eb0a485d99
solssak <gswoong1012@gmail.com>
2023-01-07 21:33:34 +0900

    로또 금액 유효성 검사 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
+import { ALERT_UNIT_MESSAGE } from "./variables.js";
+
 // 유틸 함수
+
 const $ = (selector) => document.querySelector(selector);
 
 const $showResultButton = $(".open-result-modal-button");
@@ -9,16 +12,38 @@ const $purchaseBtn = $(".lotto-purchase-btn");
 
 const $purchaseLottoInput = $(".lotto-purchase-input");
 const $purchaseLottoBtn = $(".lotto-numbers-toggle-btn");
+const $lottoSubmitForm = $(".lotto-submit-form");
 
-const purchaseLotto = () => {
-  console.log("확인");
+const handlePurchaseLottoBtn = (e) => {
   // 구입 금액 입력
+  e.preventDefault();
+  console.log("확인");
   // 유효성
+  if ($purchaseLottoInput.value == "") {
+    alert("빈값을 입력하셨습니다.");
+    return;
+  }
+
+  if ($purchaseLottoInput.value % 1000 == 0) {
+    // create lotto ticket
+    console.log("create lottos");
+  } else {
+    alert(ALERT_UNIT_MESSAGE);
+  }
   // 티켓 생성
   // 티켓 생성 함수
 };
 
-$purchaseBtn.addEventListener("click", purchaseLotto);
+const handlePurchaseLottoForm = (e) => {
+  // 구입 금액 입력
+  console.log("확인");
+  e.preventDefault();
+  // 유효성
+  // if ()
+};
+
+$purchaseBtn.addEventListener("click", handlePurchaseLottoBtn);
+$lottoSubmitForm.addEventListener("submit", handlePurchaseLottoForm);
 
 // const onModalShow = () => {
 //   $modal.classList.add("open");
