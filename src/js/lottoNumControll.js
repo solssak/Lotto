a0e3536e1f1eb0d181c4c59d418479a39e6ddcb6
solssak <gswoong1012@gmail.com>
2023-01-13 18:35:40 +0900

    docs : console.log 삭제

--- src/js/lottoNumControll.js
+++ src/js/lottoNumControll.js
@@ -1,8 +1,8 @@
 export const lottoNums = [];
+
 while (lottoNums.length < 6) {
   let lottoTicketNum = parseInt(Math.floor(Math.random() * 45) + 1);
   if (lottoNums.indexOf(lottoTicketNum) < 0) {
     lottoNums.push(lottoTicketNum);
   }
 }
-console.log(lottoNums);
